66f8f1979f807ca39e9e0a8fdaf6e33a88f6cdfd
womo333 <elimstsalew@gmail.com>
2026-03-25 04:11:16 +0800

    first

---
+++ ccds.fzu.edu.cn/assets/js/link.js
@@ -0,0 +1,48 @@
+﻿
+
+
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+		var links = this.el.find('.nav-link1');
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.sidemenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+
+	var accordion = new Accordion($('#accordion'), false);
+});
+
+
+
+
+
+function setTab(name,cursel,n){
+	for(i=1;i<=n;i++){
+		var menu=$("#"+name+i);
+		var con=$("#con_"+name+"_"+i);		
+		if(i==cursel)
+		{
+			$(menu).addClass("hover");
+			//$(con).fadeTo("slow", 1);
+			$(con).show();
+		}
+		else
+		{
+			$(menu).removeClass("hover");
+			$(con).hide();
+		}
+	}
+}
